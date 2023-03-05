b398ba7f28639d9f74e14612669eff8c4315cafa
Natalia-ITStep-2017 <detkova.n.yu@gmail.com>
2023-03-05 22:42:15 +0200

    home work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryItemsHtml = galleryItems.map(({ preview, original, description }) =>
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+  </a>
+</div>`
+).join('');
+
+galleryEl.innerHTML = galleryItemsHtml;
+
+function selectImage(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+
+  const originalUrl = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${originalUrl}">
+    `);
+  instance.show();
+  
+  if (instance.visible()) {
+     document.addEventListener("keyup", closeOriginalImage)
+  }
+
+  function closeOriginalImage(event) {
+    if (event.key === 'Escape') {
+      instance.close(() => document.removeEventListener("keyup", closeOriginalImage))
+    }
+  }
+}
+
+galleryEl.addEventListener("click", selectImage)
+
+
+
+   
+ 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryItemsHtml = galleryItems.map(({ preview, original, description }) =>
+  `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      />
+  </a>
+</div>`
+).join('');
+
+
+galleryEl.innerHTML = galleryItemsHtml;
+
+let gallery = new SimpleLightbox('.gallery a',
+  {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    showCounter: false
+  });
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
